86249300f9bc4fe87219265a9984c45dace7c8cf
Ignacio <ignacio.gutierrez@alumnos.uach.cl>
2024-11-07 19:56:47 -0300

    Funciona transmisión contínua de 0 a 5.

---
+++ CODIGO_BASE_FUNCIONANDO/CODIGO_BASE_FUNCIONANDO.ino
@@ -0,0 +1,137 @@
+#include "LoRaWan_APP.h"
+// #include "OneWireESP32.h"
+// #include <Preferences.h>
+
+/* OTAA para*/
+uint8_t devEui[] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x06, 0x53, 0xC8 };
+uint8_t appEui[] = { 0x0D, 0xDB, 0x4E, 0x05, 0xD0, 0xBF, 0x54, 0x77 };
+uint8_t appKey[] = { 0x74, 0xD6, 0x6E, 0x63, 0x45, 0x82, 0x48, 0x27, 0xFE, 0xC5, 0xB7, 0x70, 0xBA, 0x2B, 0x50, 0x45 };
+
+/* ABP para*/
+uint8_t nwkSKey[] = { 0x15, 0xB1, 0xD0, 0xEF, 0xA4, 0x63, 0xDF, 0xBE, 0x3D, 0x11, 0x18, 0x1E, 0x1E, 0xC7, 0xDA,0x85 };
+uint8_t appSKey[] = { 0xD7, 0x2C, 0x78, 0x75, 0x8C, 0xDC, 0xCA, 0xBF, 0x55, 0xEE, 0x4A, 0x77, 0x8D, 0x16, 0xEF,0x67 };
+uint32_t devAddr =  ( uint32_t )0x007E6AE1;
+
+uint16_t userChannelsMask[6]={ 0x0000,0x0000,0x0000,0x0000,0x0000,0x00FF };
+
+/*LoraWan region, select in arduino IDE tools*/
+LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
+
+/*LoraWan Class, Class A and Class C are supported*/
+DeviceClass_t  loraWanClass = CLASS_A;
+
+/*the application data transmission duty cycle.  value in [ms].*/
+uint32_t appTxDutyCycle = 15000;
+
+/*OTAA or ABP*/
+bool overTheAirActivation = true;
+
+/*ADR enable*/
+bool loraWanAdr = true;
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
+bool isTxConfirmed = true;
+
+/* Application port */
+uint8_t appPort = 2;
+/*!
+* Number of trials to transmit the frame, if the LoRaMAC layer did not
+* receive an acknowledgment. The MAC performs a datarate adaptation,
+* according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
+* to the following table:
+*
+* Transmission nb | Data Rate
+* ----------------|-----------
+* 1 (first)       | DR
+* 2               | DR
+* 3               | max(DR-1,0)
+* 4               | max(DR-1,0)
+* 5               | max(DR-2,0)
+* 6               | max(DR-2,0)
+* 7               | max(DR-3,0)
+* 8               | max(DR-3,0)
+*
+* Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
+* the datarate, in case the LoRaMAC layer did not receive an acknowledgment
+*/
+uint8_t confirmedNbTrials = 4;
+
+// *******Sensors Information*******
+RTC_DATA_ATTR int start = 0;
+
+
+/* Prepares the payload of the frame */
+static void prepareTxFrame(uint8_t port) {
+  appDataSize = 0;
+
+  if (start < 6) {
+    Serial.print("VALOR INICIAL DE START: ");
+    Serial.println(start);
+
+    appData[appDataSize++] = start;
+    appData[appDataSize++] = start + 1;
+    Serial.print(appData[0]); Serial.print(" | "); Serial.println(appData[1]);
+
+    // Incrementar 'start' aquí para que se mantenga el valor global actualizado
+    start++;
+  } else {
+    Serial.println("Reiniciando valor de start");
+    start = 0; // Reinicia start cuando llega a 6
+  }
+  Serial.println(start);
+}
+
+//if true, next uplink will add MOTE_MAC_DEVICE_TIME_REQ 
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Comenzando Serial ...");
+  Mcu.begin(HELTEC_BOARD,SLOW_CLK_TPYE);
+}
+
+void loop()
+{
+  switch( deviceState )
+  {
+    case DEVICE_STATE_INIT:
+    {
+#if(LORAWAN_DEVEUI_AUTO)
+      LoRaWAN.generateDeveuiByChipID();
+#endif
+      LoRaWAN.init(loraWanClass,loraWanRegion);
+      //both set join DR and DR when ADR off 
+      LoRaWAN.setDefaultDR(3);
+      break;
+    }
+    case DEVICE_STATE_JOIN:
+    {
+      LoRaWAN.join();
+      break;
+    }
+    case DEVICE_STATE_SEND:
+    {
+      prepareTxFrame( appPort );
+      LoRaWAN.send();
+      deviceState = DEVICE_STATE_CYCLE;
+      break;
+    }
+    case DEVICE_STATE_CYCLE:
+    {
+      // Schedule next packet transmission
+      txDutyCycleTime = appTxDutyCycle + randr( -APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND );
+      LoRaWAN.cycle(txDutyCycleTime);
+      deviceState = DEVICE_STATE_SLEEP;
+      break;
+    }
+    case DEVICE_STATE_SLEEP:
+    {
+      LoRaWAN.sleep(loraWanClass);
+      break;
+    }
+    default:
+    {
+      deviceState = DEVICE_STATE_INIT;
+      break;
+    }
+  }
+}
